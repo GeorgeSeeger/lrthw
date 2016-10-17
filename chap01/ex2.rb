10a2f4fc1574051a44a3458a741cb45e8c5eb5bd
George Seeger <georgeseeger@hotmail.com>
2016-10-17 10:01:01 +0100

    Excercise 2

---
+++ chap01/ex2.rb
@@ -0,0 +1,8 @@
+# I can write comments because I'm not a two year old
+
+puts "I could have code like this." #and the comment after is ignored
+
+#can also disable lines
+#puts "This won't run."
+
+puts "This will run."
