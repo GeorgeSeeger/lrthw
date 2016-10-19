bc99de26c621403d12223f0cc2a8540beea2fecd
George Seeger <georgeseeger@hotmail.com>
2016-10-19 10:11:57 +0100

    Excercises 3 and 4

---
+++ chap01/ex3.rb
@@ -0,0 +1,26 @@
+#prints line
+puts "I will now count my chickens:"
+
+#prints lines with string interpolation
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+#more Printing
+puts "Now I will count the eggs:"
+#prints results of calculation with precedence rules
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+#prints boolean result
+puts 3 + 2 < 5 - 7
+#demonstrates string interpolation calculation again
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+#yawn
+puts "Is it greater? #{5 > -2}"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"
